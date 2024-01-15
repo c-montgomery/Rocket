18474b5f12ac34f58e8e63fdde5a1a8cf72635c1
Collin <collinrmontgomery@proton.me>
2024-01-14 22:56:17 -0700

    init commit

---
+++ grid_sim.py
@@ -0,0 +1,67 @@
+import time
+import pynput
+
+#Constants
+GRAVITY = 9.81
+
+
+
+rows = ["","","","","","","","","",""]*10
+columns = ["","","","","","","","","",""]*10
+
+
+time_between_frame = 1/60
+position = [1,1]
+
+#total frames
+iterations = 100
+#current frame
+x = 0
+
+
+
+#print grid w/# in position
+def make_frame(position):
+    j=0
+    k=0
+    #Rows
+    for y in rows:
+        print("row: ", j)
+        j+=1    
+        #Columns
+        for x in columns:
+            if (j == position[0] and k == position[1]):
+            
+                print('#', end='')
+            else:
+                print(".", end='')
+            k+=1
+        k=0
+    
+    
+def increase_position():
+    position[0]+=1
+    position[1]+=1
+
+
+#set the x and y
+def set_position(x, y):
+    position[0] = x
+    position[1] = y
+
+
+#set frames/second
+def set_speed(fps):
+    ms_per_frame = 1/fps
+
+    return ms_per_frame
+    
+
+time_between_frame = set_speed(20)
+while (keyboard.is_pressed('q') == False):
+    
+    make_frame(position)
+    increase_position()
+    x+=1
+    time.sleep(time_between_frame)
+    
