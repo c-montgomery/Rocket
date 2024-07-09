198326c71cf81872a27c97854d7a57b2a73726b1
Collin <collinrmontgomery@proton.me>
2024-07-09 14:00:56 -0700

    add rotation w/offset compensation and throttle control

--- pygame_test/pygame_test.py
+++ pygame_test/pygame_test.py
@@ -3,7 +3,6 @@ from pygame import *
 import math
 import os
 
-
 ########################################################################################
 # Run pygame
 ########################################################################################
@@ -13,11 +12,11 @@ lighter_grey = (150,150,150)
 white = (251,251,251)
 green = (60, 255, 175)
 
-
 class SimObject:
    
     #initialze object
     def __init__(self, x_size, y_size):
+
         self.x_size = x_size    #Window size
         self.y_size = y_size    #Window size
         self.screen = pygame.display.set_mode((x_size, y_size))
@@ -26,71 +25,89 @@ class SimObject:
         self.time = self.clock.tick()
         self.elapsed = 0
 
-
-    
         #Make rocket
-        self.rocket = Rocket(3,15, 0, 0)
-        self.ship_surface = pygame.Surface((self.rocket.get_x(), self.rocket.get_y()))
-        self.shipRect = self.ship_surface.get_rect(center=(self.rocket.get_x(), self.rocket.get_y()))
+        self.rocket = Rocket(6,31, 200, 0, 0, 20)
+        self.rocket_png = pygame.image.load("rocket.png")
+        self.rocket_png.convert_alpha()
+        self.rotated_rocket = pygame.image.load("rocket.png")
+        self.shipRect = self.rocket_png.get_rect()
         self.shipRect.x = self.x_size / 2
-        self.shipRect.y = self.y_size - 1
-        self.ship_surface.fill(green)
-
-    #game loop
-    def loop(self):
+        self.shipRect.y = self.y_size - 31
+        self.y_offset = 0
+        self.x_offset = 0
         
+    #Main loop 
+    def loop(self):
 
-
-        #Main loop 
         while True:
             self.check_keypresses()
             self.update_pos()
             
             elapsed = self.clock.tick(self.FPS)
             self.elapsed += elapsed
-
-            
+        #react to user inputs       
     def check_keypresses(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     sys.exit()
+               
+
                 elif event.key == pygame.K_LEFT:
                     print("Left")
-                    degrees = self.rocket.get_rotation() + 10
-                    self.rocket.set_rotation(degrees)
-                    self.screen.fill(grey)
-                    self.ship_surface = pygame.transform.rotate(self.ship_surface, degrees)        
+                    self.maintain_center(0)
+                    # self.rocket_png = pygame.transform.rotate(self.rocket_png, degrees)    
+
                 elif event.key == pygame.K_RIGHT:
                     print("RIGHT")
-                    degrees = self.rocket.get_rotation() - 10
-                    self.rocket.set_rotation(degrees)
-                    self.screen.fill(grey)
-                    self.ship_surface = pygame.transform.rotate(self.ship_surface, degrees)
-
+                    
+                    self.maintain_center(1)
+                    #pygame.display.flip()
+               
                 elif event.key == pygame.K_UP:
                     print("THROTTLE UP")
+                    current_throttle = self.rocket.get_throttle()
+                    if int(current_throttle) < 100:
+                        self.rocket.set_throttle(current_throttle +10)
                 elif event.key == pygame.K_DOWN:
                     print("THROTTLE DOWN")
+                    if self.rocket.get_throttle() > 0:
+                        self.rocket.set_throttle(current_throttle - 10)
+        #rotates image about center. rotates like jumping bean w/o this
+    def maintain_center(self, direction ):
+                degrees = self.rocket.get_rotation()
+                if (direction ==1):
+                    degrees -= 10
+                    self.rocket.set_rotation(degrees)
+                else:
+                    degrees += 10
+                    self.rocket.set_rotation(degrees)
+                self.shipRect = self.rocket_png.get_rect()
+                self.screen.fill(grey)
+                x,y = self.shipRect.center
+                self.rotated_rocket = pygame.transform.rotate(self.rocket_png, degrees) 
+                self.x_offset, self.y_offset = self.rotated_rocket.get_rect().center
+                self.shipRect.center = (x ,y - self.y_offset)
+                
+            
+        #Draw bounding rectangle
+    def draw_rectangle(self, object):
+        pygame.draw.rect(object, (255,255,255), object.get_bounding_rect(), width = 1)
 
-  
-        
+        #fill screen, update image and rectangle placement. Display changes
     def update_pos(self):
-        self.shipRect.y = self.y_size - math.floor((self.elapsed / 1000)*(1.2**6))
+        self.shipRect.y = self.y_size - (30 + math.floor((self.elapsed/ 500)**1.4 *(1.2**6)))
+        #print(self.x_offset)
+        if self.rocket.get_rotation() != 0:
+            self.shipRect.x =  200  -(self.x_offset)  
+        else:
+            self.shipRect.x = 197
+        #self.screen.blit(self.rocket_png, (self.shipRect.center[0], self.shipRect.y - (self.rocket.get_height()*2 )))
         self.screen.fill(grey)
-        #shipRect.fill(0, 125, 0)
- 
-        self.screen.blit(self.ship_surface, self.shipRect)
-        #self.screen.fill(lighter_grey)
-        # ship = pygame.Surface((3,12))
-        # rotated_ship = pygame.transform.rotate(ship, 45)
-        # rotated_rect = rotated_ship.get_rect()
-       
-
-        #self.screen.blit(rotated_ship, rotated_rect )
+        self.screen.blit(self.rotated_rocket, self.shipRect)
         pygame.display.flip()
-
+        #self.draw_rectangle(self.screen)
 
 
 ########################################################################################
@@ -98,12 +115,14 @@ class SimObject:
 ########################################################################################
 
 class Rocket:
-    def __init__(self, x, y, rotation, thrust, mass=100, fuel=999, max_fuel=999, max_thrust=1000):
+    def __init__(self, height, width, x, y, rotation, throttle, mass=100, fuel=999, max_fuel=999, max_thrust=100):
         self.x = x
         self.y = y
-        self.rocket_png = pygame.image.load("rocket.png")
+        self.height = height
+        self.width = width
+        self.center = (0,0)
         self.rotation = rotation
-        self.thrust = thrust
+        self.throttle = throttle
         self.mass = mass
         self.fuel = fuel
         self.max_fuel = max_fuel
@@ -116,34 +135,41 @@ class Rocket:
         self.y = y
     def set_rotation(self, rotate):
         self.rotation = rotate
-    def set_thrust(self, thrust):
-        self.thrust = thrust
+    def set_throttle(self, throttle):
+        self.throttle = throttle
     def set_fuel(self, fuel):
         self.fuel = fuel
+    def set_height(self, height):
+        self.height = height
+    def set_width(self, width):
+        self.width = width
+    def set_center(self, center):
+        self.center = center
 
     # GETTERS
     def get_x(self):
         return self.x
     def get_y(self):
         return self.y
+    def get_width(self):
+        return self.width
+    def get_height(self):
+        return self.height
+    def get_weight(self):
+        return self.weight
     def get_rotation(self):
         return self.rotation
-    def get_thrust(self):
-        return self.thrust
+    def get_throttle(self):
+        return self.throttle
     def get_mass(self):
         return self.mass
     def get_fuel(self):
         return self.fuel
+    def get_center(self, center):
+        return self.center
         
 
-
-
-
-
-
-
-
-simObj = SimObject(500,400)
+simObj = SimObject(480,640)
 simObj.loop()
 
 
