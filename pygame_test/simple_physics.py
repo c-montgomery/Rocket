8b9a230ef5ff03872974b98166261ede83f359ea
Collin M <collinrmontgomery@proton.me>
2024-07-15 14:36:41 -0700

    1D physics test

--- pygame_test/simple_physics.py
+++ pygame_test/simple_physics.py
@@ -1,12 +1,13 @@
 import time
-
+import pygame
+import scipy
 #projectile
 
 class projectile:
     
     def __init__(self, mass=1, f=0, x=0, y=0, v=0, v_final=0, a=0, g=-9.81):
         self.mass = 1 #kg
-        self.f = 0
+        self.f = 1000 #N
         self.x = 0
         self.y = 0
         self.v = 0
@@ -25,14 +26,14 @@ class projectile:
 
     def output(self, height, mg):
         print()
+        print("height " + str(round((self.y),4)))
         print( "throttle " + str(self.throttle))
-        print( "mass x grav " + str(self.mg))
-        print(self.y)
-        print("height " + str(round((1000 *self.y),4)))
+        print("Propulsion", self.propulsion)
         print("v_initial "+ str(self.v))
         print("v_final "+ str(self.v_final))
         print("net+accel " + str(self.net_accel))
-      #print("====================")
+        
+        print("====================")
 
     def manip_throttle(self, val):
          self.throttle = val
@@ -40,7 +41,11 @@ class projectile:
     def run(self):
         self.setup()
         #
+        output = 0
+        print("ran run")
         while(True):
+            
+           
             self.time_segment = time.time() - self.last_time
             self.elapsed_total = time.time() - self.time_start
             self.last_time = self.time_start + self.elapsed_total
@@ -48,26 +53,43 @@ class projectile:
             #Forces
             self.mg = self.mass * self.g
             self.propulsion =  self.thrust * self.throttle 
+            
             self.net_accel = self.mg + self.propulsion
-            if round(self.elapsed_total, 1) % .50 == 0:
+            if round(self.elapsed_total, 1) % 1 == 0: #and round(self.elapsed_total,1)> output:
                 self.output(self.y,self.mg)
+                print(output)
+                output+=1
+                if self.y >= 60000:
+                    
+                    self.manip_throttle(1)
 
             #velocity
             self.v_final = self.v + self.net_accel * (self.time_segment)
             self.v = self.v_final
-
+            self.vector()
             #distance
-            self.y = self.v_final * (self.time_segment) + (.5 * self.net_accel * (self.time_segment)**2)
+            if (self.v_final < 10 and self.y <= 0):
+                self.y = 0
+            else:
+                self.y +=(self.v_final * (self.time_segment) + (.5 * self.net_accel * (self.time_segment)**2))
+            time.sleep(.2)
             
+
+    def vector(self):
+
+        v = pygame.math.Vector2()
+        v.xy = 3, 4
+        
 
 
             
 
     def setup(self):
-        self.manip_throttle(10)
+        self.manip_throttle(15)
         self.time_start = time.time()
 
 
 flying_rock = projectile()
 flying_rock.setup()
 flying_rock.run()
+flying_rock.vector()
